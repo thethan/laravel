a37e862fe05ecb49f45f294dedcd9f9cf4eeacbd
Ethan Totten <ethan.totten@gmail.com>
2019-06-25 16:54:36 -0700

    Adding Stdout to logs

--- config/logging.php
+++ config/logging.php
@@ -71,6 +71,15 @@ return [
             ],
         ],
 
+         'stdout' => [
+            'driver' => 'monolog',
+            'handler' => StreamHandler::class,
+            'formatter' => env('LOG_STDOUT_FORMATTER'),
+            'with' => [
+                'stream' => 'php://stdout',
+            ],
+        ],
+        
         'stderr' => [
             'driver' => 'monolog',
             'handler' => StreamHandler::class,
